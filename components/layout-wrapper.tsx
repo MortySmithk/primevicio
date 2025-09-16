325a97ce5b95d6ec92db66fe180235b2d3ad279d
Morty Smith <faseu81@gmail.com>
2025-09-16 01:54:44 -0300

    Fix: Corrige importação do Header

--- components/layout-wrapper.tsx
+++ components/layout-wrapper.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import { Header } from "@/components/header"
+import { Header } from "@/components/Header"
 
 export function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
